01d82126a237c46b5861eebe80cebd0836afffda
Noel Pinto <noelpinto47@Noels-MacBook-Air.local>
2024-08-19 21:09:04 +0530

    added projects and logs

--- routes/routes.js
+++ routes/routes.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 const userRoutes = require('./user.routes');
 const companyRoutes = require('./company.routes');
+const projectRoutes = require('./project.routes');
+const logRoutes = require('./log.routes');
 
 // Register sub-routes
 router.use('/api/users', userRoutes)
 router.use('/api/company', companyRoutes)
+router.use('/api/project', projectRoutes)
+router.use('/api/log', logRoutes)
 
 module.exports = router;
