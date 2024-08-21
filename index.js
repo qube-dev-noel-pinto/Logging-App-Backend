f5e56d80a938f5a1bdf4389f11cb9a0155c42b0a
Noel Pinto <noelpinto47@Noels-MacBook-Air.local>
2024-08-21 10:09:01 +0530

    added cors

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 const routes = require('./routes/routes');  // Import top-level routes
 require('./config/db');  // Connect to MongoDB
+// Example in Express.js
+const cors = require('cors');
 
+app.use(cors({
+  origin: '*',
+}));
 app.use(express.json());  // Middleware for parsing JSON
 app.use(routes);          // Use top-level routes
 
